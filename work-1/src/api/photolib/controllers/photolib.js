388279209bb684f98db80bd7fad688402aa7acea
abdhu2 <abdhu2628@gmail.com>
2022-06-30 20:09:28 +0530

    first commit

---
+++ work-1/src/api/photolib/controllers/photolib.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  photolib controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::photolib.photolib');
---
+++ work-1/src/api/photolib/routes/photolib.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * photolib router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::photolib.photolib');
